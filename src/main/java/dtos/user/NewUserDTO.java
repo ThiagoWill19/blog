f17bf7888b64b4dabd30a5052df584ceaefef7b6
ThiagoWill19 <thiagopompeu19@gmail.com>
2025-10-01 18:41:20 -0300

    NewUserDTO created

---
+++ src/main/java/dtos/user/NewUserDTO.java
@@ -0,0 +1,15 @@
+package dtos.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class NewUserDTO {
+
+	private String name;
+	private String email;
+	private String password;
+}
