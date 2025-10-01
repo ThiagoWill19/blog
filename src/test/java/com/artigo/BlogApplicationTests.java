821d3975d5f5c709aabb7e19e9286d4f3d23799e
ThiagoWill19 <thiagopompeu19@gmail.com>
2025-10-01 16:33:08 -0300

    First commit

---
+++ src/test/java/com/artigo/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.artigo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
